de2831bec593f8f381686e43853b23dbc265589a
devolonter <devolonter@gmail.com>
2023-04-21 16:18:39 +0200

    Added support time uniform for ShaderView widget

--- lib/common/shader_view.dart
+++ lib/common/shader_view.dart
@@ -1,14 +1,18 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class ShaderPainter extends CustomPainter {
-  final FragmentShader shader;
-  final Paint _paint;
+  late final Paint _paint;
 
   ShaderPainter({
-    required this.shader,
-  }): _paint = Paint()..shader = shader;
+    required FragmentShader shader,
+    Listenable? repaint,
+  }) : super(repaint: repaint) {
+    _paint = Paint()..shader = shader;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -20,32 +24,56 @@ class ShaderPainter extends CustomPainter {
 }
 
 class ShaderView extends StatefulWidget {
-  const ShaderView({Key? key, required this.shaderName}) : super(key: key);
-
   final String shaderName;
+  final int? timeUniform;
+
+  const ShaderView({Key? key, required this.shaderName, this.timeUniform})
+      : super(key: key);
 
   @override
   State<ShaderView> createState() => _ShaderViewState();
 }
 
-class _ShaderViewState extends State<ShaderView> {
-  late Future<FragmentProgram> loader;
+class _ShaderViewState extends State<ShaderView>
+    with SingleTickerProviderStateMixin {
+  late final Future<FragmentProgram> _loader;
+
+  FragmentShader? _shader;
+  ValueNotifier<double>? _time;
+  Ticker? _ticker;
 
   @override
   void initState() {
     super.initState();
-    loader = FragmentProgram.fromAsset("shaders/${widget.shaderName}.frag");
+    _loader = FragmentProgram.fromAsset("shaders/${widget.shaderName}.frag");
+
+    if (widget.timeUniform != null) {
+      _time = ValueNotifier(0.0);
+      _ticker = createTicker((elapsed) {
+        final double elapsedSeconds = elapsed.inMilliseconds / 1000;
+        _shader?.setFloat(0, elapsedSeconds);
+        _time?.value = elapsedSeconds;
+      });
+      _ticker!.start();
+    }
+  }
+
+  @override
+  void dispose() {
+    _ticker?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<FragmentProgram>(
-      future: loader,
+      future: _loader,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
+          _shader = snapshot.data!.fragmentShader();
+
           return CustomPaint(
-            painter: ShaderPainter(shader: snapshot.data!.fragmentShader()),
-          );
+              painter: ShaderPainter(shader: _shader!, repaint: _time));
         } else {
           if (snapshot.hasError) {
             print(snapshot.error);
