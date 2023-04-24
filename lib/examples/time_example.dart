7e212c5330c9a46b88992e2610e1b78a6dbf3450
devolonter <devolonter@gmail.com>
2023-04-24 13:15:31 +0200

    Final version of time example

--- lib/examples/time_example.dart
+++ lib/examples/time_example.dart
@@ -17,8 +17,23 @@ class _TimeExampleState extends State<TimeExample> {
 
   @override
   Widget build(BuildContext context) {
-    return const ShaderView(
-        shaderName: 'time',
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 1.0
+        ),
+        itemCount: 10,
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return ShaderView(
+            shaderName: 'time',
+            onShaderLoaded: (setUniform) {
+              setUniform('uOffset', index / 10.0);
+            }
+          );
+        },
     );
   }
 }
--- shaders/time.frag
+++ shaders/time.frag
@@ -1,7 +1,8 @@
 out vec4 fragColor;
 
 uniform float uTime;
+uniform float uOffset;
 
 void main() {
-    fragColor = vec4(1.0, 0.0, 1.0, 1.0) * fract(uTime);
+    fragColor = vec4(1.0, 0.0, 1.0, 1.0) * fract(uTime - uOffset);
 }
